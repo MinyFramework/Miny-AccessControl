46197f898569f6d69e8ccbbc6b4d1ce9fb67d56a
Buga Dániel <bugadani@gmail.com>
2014-07-02 19:33:26 +0200

    Update composer: don't depend on Miny-Annotation. Update LICENSE date, travis config. Change email.

--- src/RoleContainer.php
+++ src/RoleContainer.php
@@ -1,7 +1,8 @@
 <?php
+
 /**
  * This file is part of the Miny framework.
- * (c) Dániel Buga <daniel@bugadani.hu>
+ * (c) Dániel Buga <bugadani@gmail.com>
  *
  * For licensing information see the LICENSE file.
  */
